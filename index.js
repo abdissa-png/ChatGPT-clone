b0f60eb1de4209df8fcb2200fc31a53e8de87659
abdissa <abdissadegefu1@gmail.com>
2023-03-09 16:47:25 +0300

    index

--- index.js
+++ index.js
@@ -72,8 +72,7 @@ const handleSubmit = async(e) => {
 
     loader(messageDiv);
 
-    const response = await
-    fetch('https://gptclone-2zgb.onrender.com', {
+    const response = await fetch('https://gptclone-2zgb.onrender.com', {
         method: 'POST',
         referrer: "no-referrer-when-downgrade",
         headers: {
