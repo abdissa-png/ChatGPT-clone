4ee1531e151c784777b2ed74ba2825f4bb577cec
abdissa <abdissadegefu1@gmail.com>
2023-03-09 16:56:46 +0300

    referrer policy

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const handleSubmit = async(e) => {
 
     const response = await fetch('https://gptclone-2zgb.onrender.com', {
         method: 'POST',
-        referrer: "no-referrer-when-downgrade",
+        referrerPolicy: "no-referrer-when-downgrade",
         headers: {
             "Content-type": "application/json"
         },
